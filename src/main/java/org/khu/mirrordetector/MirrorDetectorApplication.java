d1118081d07708c11edcd923b397f077d2e5c43b
morteza_mhzd <morteza363831official@gmail.com>
2026-06-09 21:38:04 +0330

    initial project

---
+++ src/main/java/org/khu/mirrordetector/MirrorDetectorApplication.java
@@ -0,0 +1,13 @@
+package org.khu.mirrordetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirrorDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MirrorDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/khu/mirrordetector/MirrorDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khu.mirrordetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirrorDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
