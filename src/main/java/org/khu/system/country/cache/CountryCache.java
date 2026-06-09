d8468e9ce03c836b595dc42a29552d183cc7e218
morteza_mhzd <morteza363831official@gmail.com>
2026-06-09 23:57:12 +0330

    country cache added

---
+++ src/main/java/org/khu/system/country/cache/CountryCache.java
@@ -0,0 +1,11 @@
+package org.khu.system.country.cache;
+
+import org.khu.system.country.domain.model.Country;
+
+import java.util.List;
+
+public interface CountryCache {
+
+    List<Country> countries();
+
+}
