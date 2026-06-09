0017e7f7c6fa52851c2f2aab3ec1aa88f7cc5f77
morteza_mhzd <morteza363831official@gmail.com>
2026-06-10 00:00:51 +0330

    injection completed in service and controller

--- src/main/java/org/khu/system/country/controller/CountryController.java
+++ src/main/java/org/khu/system/country/controller/CountryController.java
@@ -1,4 +1,17 @@
 package org.khu.system.country.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.khu.system.country.service.CountryService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/countries")
+@RequiredArgsConstructor
 public class CountryController {
+
+    // services
+    private final CountryService countryService;
+
+
 }
--- src/main/java/org/khu/system/country/service/CountryServiceImpl.java
+++ src/main/java/org/khu/system/country/service/CountryServiceImpl.java
@@ -1,4 +1,21 @@
 package org.khu.system.country.service;
 
-public class CountryServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.khu.system.country.cache.CountryCache;
+import org.khu.system.country.domain.mapper.CountryMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CountryServiceImpl implements CountryService {
+
+    // tools
+    private final CountryMapper countryMapper;
+
+    // cache
+    private final CountryCache countryCache;
+
+
+
+
 }
