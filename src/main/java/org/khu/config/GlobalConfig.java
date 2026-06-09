5bba72e2d3ad56f7544ad270b4eddd121517fa26
morteza_mhzd <morteza363831official@gmail.com>
2026-06-09 23:07:32 +0330

    global config added

---
+++ src/main/java/org/khu/config/GlobalConfig.java
@@ -0,0 +1,22 @@
+package org.khu.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+
+@Configuration
+public class GlobalConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder().build();
+    }
+
+}
