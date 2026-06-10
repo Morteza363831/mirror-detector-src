33860a3b2c36dc6d97f90c8af58d163127d19b44
morteza_mhzd <morteza363831official@gmail.com>
2026-06-11 00:18:49 +0330

    request handler replaced with distribution cache

--- src/main/java/org/khu/system/distribution/service/DistributionServiceImpl.java
+++ src/main/java/org/khu/system/distribution/service/DistributionServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.khu.system.distribution.cache.DistributionCache;
 import org.khu.system.distribution.domain.dto.DistributionResultDto;
 import org.khu.system.distribution.domain.mapper.DistributionMapper;
-import org.khu.system.distribution.handler.DistributionRequestHandler;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +14,6 @@ public class DistributionServiceImpl implements DistributionService {
     private final DistributionMapper distributionMapper;
 
     // handlers
-    private final DistributionRequestHandler distributionRequestHandler;
     private final DistributionCache distributionCache;
 
 }
