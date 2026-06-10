cc2d6baee7ea563eb7b3d389ec383ebb734ca525
morteza_mhzd <morteza363831official@gmail.com>
2026-06-10 22:40:00 +0330

    jsr dependency added for time handling in json

--- src/main/java/org/khu/config/GlobalConfig.java
+++ src/main/java/org/khu/config/GlobalConfig.java
@@ -1,6 +1,8 @@
 package org.khu.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,15 @@ public class GlobalConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.registerModule(new JavaTimeModule());
+
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
+
     }
 
     @Bean
