d4eed821b6ca3ed54028818263ce5c6bcc7e267c
morteza_mhzd <morteza363831official@gmail.com>
2026-06-12 17:14:37 +0330

    error and debug logging notation and apcets added

---
+++ src/main/java/org/khu/logging/DebugLoggingAspect.java
@@ -0,0 +1,49 @@
+package org.khu.logging;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.khu.utils.LogSummaryUtil;
+import org.springframework.stereotype.Component;
+
+@ErrorLogging
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class DebugLoggingAspect {
+
+
+    @Pointcut("@annotation(DebugLogging) || @within(DebugLogging)")
+    public void onDebug() {}
+
+    @Around("onDebug()")
+    public Object debug(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        Class<?> clazz = joinPoint.getTarget().getClass();
+
+        String method = joinPoint.getSignature().getName();
+
+        Object[] args = joinPoint.getArgs();
+
+        long start = System.currentTimeMillis();
+
+       // LoggingUtil.debug(clazz, method, "Before processing method", "Before processing method");
+
+        LoggingUtil.debug(clazz, method + " INPUT", args);
+
+
+        Object result = joinPoint.proceed();
+
+        long duration = System.currentTimeMillis() - start;
+
+        LoggingUtil.debug(clazz, method + "OUTPUT", LogSummaryUtil.summarize(result));
+
+        LoggingUtil.debug(clazz, method + "DURATION", duration + " ms");
+
+        return result;
+
+    }
+
+}
